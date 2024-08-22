3080c32ee6aa78e02aff343dccd650b43ffe2da9
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-08-22 08:46:11 +0530

    fixing error in printing

--- endpoints/getFoodItems.go
+++ endpoints/getFoodItems.go
@@ -1,8 +1,6 @@
 package endpoints
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/surya-nara0123/swiftship/database"
 	"github.com/surya-nara0123/swiftship/types"
@@ -13,7 +11,6 @@ func GetFoodItems(c *fiber.Ctx, DbInterface database.DatabaseStruct) error {
 
 	foodItems := []types.FoodItems{}
 	db.Find(&foodItems)
-	fmt.Println(foodItems[0])
 
 	return c.JSON(fiber.Map{
 		"food_items": foodItems,
